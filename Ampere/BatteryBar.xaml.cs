998cd89124203f85d86780f68348e4f94ace2cbd
Sharath <sharathnimala@gmail.com>
2021-10-22 20:52:42 +0530

    First Upload

---
+++ Ampere/BatteryBar.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ampere
+{
+    /// <summary>
+    /// Interaction logic for BatteryBar.xaml
+    /// </summary>
+    public partial class BatteryBar : UserControl
+    {
+        public BatteryBar()
+        {
+            InitializeComponent();
+        }
+
+        public void ProgressBarValue(float value)
+        {
+            ProgressValue.Height = 243 * (value / 100);
+
+            if (value > 50)
+            {
+                ProgressValue.Fill = (Brush)(new BrushConverter().ConvertFrom("#35a0f6"));
+                blurEffect.Color = Color.FromRgb(53, 160, 246); 
+            }
+            else if (value <= 50 && value >= 15)
+            {
+                ProgressValue.Fill = (Brush)(new BrushConverter().ConvertFrom("#ffd40e"));
+                blurEffect.Color = Color.FromRgb(255, 212, 14);
+            }
+            else if (value <= 15 && value >= 0)
+            {
+                ProgressValue.Fill = (Brush)(new BrushConverter().ConvertFrom("#ff1000"));
+                blurEffect.Color = Color.FromRgb(255, 16, 0);
+            }
+        }
+    }
+}
---
+++ Ampere/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Ampere
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Forms.PowerStatus pwr;
+        float i = 100;
+        public MainWindow()
+        {
+            InitializeComponent();
+            pwr = System.Windows.Forms.SystemInformation.PowerStatus;
+            batteryBar.ProgressBarValue(GetBatteryPerc());
+            DispatcherTimer mainTimer = new DispatcherTimer();
+            mainTimer.Interval = TimeSpan.FromMilliseconds(50);
+            mainTimer.Tick += MainTimer_Tick;
+            mainTimer.Start();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            batteryBar.ProgressBarValue(GetBatteryPerc());
+        }
+
+        private float GetBatteryPerc()
+        {
+            return pwr.BatteryLifePercent * 100;
+        }
+
+        private void clButtonGrid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            clButtonM.Fill = (Brush)(new BrushConverter().ConvertFrom("#ffd40e"));
+            clButtonP.Fill = (Brush)(new BrushConverter().ConvertFrom("#ffd40e"));
+        }
+
+        private void clButtonGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            clButtonM.Fill = (Brush)(new BrushConverter().ConvertFrom("#FF303030"));
+            clButtonP.Fill = (Brush)(new BrushConverter().ConvertFrom("#FF303030"));
+        }
+
+        private void clButtonM_MouseEnter(object sender, MouseEventArgs e)
+        {
+            clButtonM.Fill = (Brush)(new BrushConverter().ConvertFrom("#ffd40e"));
+            clButtonP.Fill = (Brush)(new BrushConverter().ConvertFrom("#ffd40e"));
+        }
+
+        private void clButtonM_MouseLeave(object sender, MouseEventArgs e)
+        {
+            clButtonM.Fill = (Brush)(new BrushConverter().ConvertFrom("#FF303030"));
+            clButtonP.Fill = (Brush)(new BrushConverter().ConvertFrom("#FF303030"));
+        }
+
+        private void clButtonP_MouseEnter(object sender, MouseEventArgs e)
+        {
+            clButtonM.Fill = (Brush)(new BrushConverter().ConvertFrom("#ffd40e"));
+            clButtonP.Fill = (Brush)(new BrushConverter().ConvertFrom("#ffd40e"));
+        }
+
+        private void clButtonP_MouseLeave(object sender, MouseEventArgs e)
+        {
+            clButtonM.Fill = (Brush)(new BrushConverter().ConvertFrom("#FF303030"));
+            clButtonP.Fill = (Brush)(new BrushConverter().ConvertFrom("#FF303030"));
+        }
+
+        private void clButtonGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
